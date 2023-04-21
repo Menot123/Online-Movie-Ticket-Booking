9c938bcf3209b809254bfc1493e0249baf9a568b
VuLuongNgocBan <vuban2662002@gmail.com>
2023-04-21 21:25:53 +0700

    them du lieu cho binh luan phim & blog dien anh + hoan thien trang chu

--- src/app/controllers/homeController.js
+++ src/app/controllers/homeController.js
@@ -22,7 +22,7 @@ async function index(req, res, next) {
     try {
         const list = await homeServices.getListNotifications()
         const blogs = await homeServices.getBlogs()
-        const films = await nowShowingServices.get6NowShowingFilm()
+        const films = await nowShowingServices.getNowShowingFilm()
         res.render('home', {
             data: list,
             blogs: blogs,
